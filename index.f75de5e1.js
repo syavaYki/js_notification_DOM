6a39ba3bd5fe2d75a1d8b16a18351ddc0f1a5b55
Seva Podolskiy <s_podolskiy@live.com>
2024-07-05 13:33:55 -0400

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,24 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const messageElem = document.createElement("div");
+    const titleEl = document.createElement("h2");
+    const descr = document.createElement("p");
+    messageElem.classList.add("notification", type);
+    messageElem.style.top = posTop + "px";
+    messageElem.style.right = posRight + "px";
+    titleEl.textContent = title;
+    titleEl.style.whiteSpace = "nowrap";
+    titleEl.classList.add("title");
+    descr.textContent = description;
+    messageElem.append(titleEl);
+    messageElem.append(descr);
+    document.querySelector("body")?.append(messageElem);
+    setTimeout(()=>{
+        messageElem.style.visibility = "hidden";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
